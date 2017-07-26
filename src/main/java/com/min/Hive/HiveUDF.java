4d1dd167e6a60785521a896df5bccb0995719218
PC-20170304HXPM\Administrator <654279133@qq.com>
2017-07-26 17:18:37 +0800

    UDF

--- src/main/java/com/min/Hive/HiveUDF.java
+++ src/main/java/com/min/Hive/HiveUDF.java
@@ -1,18 +1,37 @@
 package com.min.Hive;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.hadoop.hive.ql.exec.UDF;
+
 /*
  * 定义一个类继承UDF，添加方法
  * evaluate（）参数和返回值和函数的输入输出一致
  * 把项目打成jar包，用hive加载
  * 调用函数：hive新建一个function，指定到输出
  */
-public class HiveUDF extends UDF{
+public class HiveUDF extends UDF {
 	/*
-	 * 接收一个被格式化的日期类型的字符串的参数
+	 * 接收一个被格式化的日期类型的字符串的参数 在方法里将它提取出来，更改格式
 	 */
-public static void evaluate() {
-	
-}
-	
+	public static String evaluate(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MMMM/yyyy hh:mm:ss Z");
+		if (date.indexOf("[") > -1) {
+			date = date.replace("[", "");
+		}
+		if (date.indexOf("]") > -1) {
+			date = date.replace("]", "");
+		}
+		try {
+			Date parse = format.parse(date);
+			return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(parse);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 }
