10ebc35f927ad6fd382c8036b944eaa8df33631f
PC-20170304HXPM\Administrator <654279133@qq.com>
2017-07-27 17:02:13 +0800

    自定义UDAF，求平均值

---
+++ src/main/java/com/min/Hive/AvgUDAF.java
@@ -0,0 +1,50 @@
+package com.min.Hive;
+
+import org.apache.hadoop.hive.ql.exec.UDAF;
+import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+
+public class AvgUDAF extends UDAF {
+	public static class State {
+		private double sum;
+		private int count;
+
+	}
+
+	public static class AvgValueUDAF implements UDAFEvaluator {
+
+		private State state = new State();
+
+		public void init() {
+			// TODO Auto-generated method stub
+			state.count = 0;
+			state.sum = 0;
+		}
+
+		public boolean iterate(Double value) {
+			if (value != null) {
+				state.sum += value;
+				state.count++;
+			}
+			return true;
+		}
+
+		public State terminatePartial() {
+			return state.count == 0 ? null : state;
+		}
+
+		public boolean merge(State o) {
+			if (o != null) {
+				state.count += o.count;
+				state.sum += o.sum;
+			}
+			return true;
+		}
+
+		// 返回最终结果
+		public Double terminate() {
+			return state.count == 0 ? null : Double.valueOf(state.sum / state.count);
+		}
+	}
+}
